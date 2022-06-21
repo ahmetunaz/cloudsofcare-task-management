37360c7e63ea3ac74529133fb802779550876cd5
ahmetunaz <54167776+ahmetunaz@users.noreply.github.com>
2022-06-21 19:42:51 +0200

    Assigned to and Date columns are added to task list table

--- src/pages/Tasks/taskList.js
+++ src/pages/Tasks/taskList.js
@@ -2,10 +2,11 @@ import React from "react";
 import PropTypes from "prop-types";
 import { SimpleTable } from "components/SimpleTable";
 import { useDataSource } from "hooks/useDataSource";
-import { getTasks } from "store/actions";
+import { getTasks, getUsers } from "store/actions";
 import { TaskDelete } from "./taskDelete";
 import Loading from "components/Loading/Index";
 import { Card, CardBody } from "reactstrap";
+import moment from "moment";
 
 const tableColumns = [
   {
@@ -19,6 +20,15 @@ const tableColumns = [
   {
     key: "description",
     value: "Description",
+  },
+  {
+    key: "assigned_to",
+    value: "Assigned To",
+  },
+  {
+    key: "created_at",
+    value: "Date",
+    formatter: (row, value) => moment(value).format("DD/MM/YYYY"),
   },
   {
     key: "is_completed",
@@ -43,13 +53,22 @@ const tableColumns = [
 
 export const TaskList = ({ onRowClick }) => {
   const { tasks, loading } = useDataSource(getTasks, state => state.TaskState);
+  const { users } = useDataSource(getUsers, state => state.UserState);
 
   return (
     <Card>
       <CardBody>
         <SimpleTable
           columns={tableColumns}
-          rows={tasks}
+          rows={tasks.map(task => {
+            const taskUser =
+              users.length > 0 &&
+              users.find(user => user.id === task.assigned_to);
+            return {
+              ...task,
+              assigned_to: taskUser?.name || task.assigned_to,
+            };
+          })}
           onRowClick={onRowClick}
         />
         {loading && <Loading />}
