d67293b508b6c8e9f488cfd18e5325772cd5eba2
ahmetunaz <54167776+ahmetunaz@users.noreply.github.com>
2022-06-22 12:53:04 +0200

    Clouds of care logo & favicon are added

--- src/components/VerticalLayout/Sidebar.js
+++ src/components/VerticalLayout/Sidebar.js
@@ -1,30 +1,28 @@
-import React, { Component } from "react"
-import PropTypes from "prop-types"
-import { connect } from "react-redux"
-import { withRouter } from "react-router-dom"
-import { } from "../../store/actions"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import {} from "../../store/actions";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 //i18n
-import { withTranslation } from "react-i18next"
-import SidebarContent from "./SidebarContent"
+import { withTranslation } from "react-i18next";
+import SidebarContent from "./SidebarContent";
 
-import logo from "../../assets/images/logo.svg"
-import logoLightPng from "../../assets/images/logo-light.png"
-import logoLightSvg from "../../assets/images/logo-light.svg"
-import logoDark from "../../assets/images/logo-dark.png"
+import logo from "../../assets/images/clouds-of-care-logo-white.svg";
+import logoLightSvg from "../../assets/images/clouds-of-care-icon-white.svg";
+import logoDark from "../../assets/images/logo-dark.png";
 
 class Sidebar extends Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   render() {
     return (
       <React.Fragment>
-
         <div className="vertical-menu">
           <div className="navbar-brand-box">
             <Link to="/" className="logo logo-dark">
@@ -41,7 +39,7 @@ class Sidebar extends Component {
                 <img src={logoLightSvg} alt="" height="22" />
               </span>
               <span className="logo-lg">
-                <img src={logoLightPng} alt="" height="19" />
+                <img src={logo} alt="" height="19" />
               </span>
             </Link>
           </div>
@@ -55,20 +53,20 @@ class Sidebar extends Component {
           <div className="sidebar-background"></div>
         </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
 Sidebar.propTypes = {
   type: PropTypes.string,
-}
+};
 
 const mapStateToProps = state => {
   return {
     layout: state.Layout,
-  }
-}
+  };
+};
 export default connect(
   mapStateToProps,
   {}
-)(withRouter(withTranslation()(Sidebar)))
+)(withRouter(withTranslation()(Sidebar)));
